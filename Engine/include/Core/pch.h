af275793706f8caa803ea5a226d4ac4ac234837b
Paradogx <kiritoshikahara@gmail.com>
2026-02-12 11:48:27 +0900

    projectの追加

---
+++ Engine/include/Core/pch.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+//	Windows.hの無駄削除
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN 
+#endif
+
+#define NOMINMAX
+#include <windows.h>
+
+// DirectX12
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <wrl.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+
+// STL
+#include<vector>
+#include<array>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+
+#include<string>
+#include<string_view>
+#include<filesystem>
+
+#include<memory>
+#include<optional>
+#include<variant>
+#include<concepts>
+#include<coroutine>
+
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<tuple>
+#include<span>
+#include<ranges>
+#include<initializer_list>
+
+#include<thread>
+#include<mutex>
+#include<atomic>
+
+#include<iostream>
+#include<sstream>
+#include<fstream>
+
+
+
+//	ホットリロード実装する可能性あるので前提で組みます。
+#ifdef MYENGINE_AS_DLL
+//	Dllの時
+#ifdef MYENGINE_EXPORTS
+#define ENGINE_API __declspec(dllexport)
+#else
+#define ENGINE_API __declspec(dllimport)
+#endif
+#else
+//	libの時は何もしない
+#define ENGINE_API	
+#endif
+
+
+
+
+
+namespace Ecse
+{
+
+}
+
+//	STL
+
+
+#endif //PCH_H
---
+++ Game/Src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include<Windows.h>
+
+#if _DEBUG
+#define _CRTDBG_MAP_ALLOC 
+#include <crtdbg.h>
+#endif // _DEBUG
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	return 0;
+}
