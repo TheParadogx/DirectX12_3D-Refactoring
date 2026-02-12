f1a296cdeb7bdb5c2d7e9773fd120b29d58a5fe9
Paradogx <kiritoshikahara@gmail.com>
2026-02-12 13:26:20 +0900

    ImGuiの追加

---
+++ Engine/External/Plugin/ImGui/imgui_win32_forward.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "imgui.h"
+#include <windows.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
---
+++ Engine/include/Core/EcseTypes.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+
+namespace Ecse
+{
+	template<typename T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+	// デバイス・基盤
+	using Device = ComPtr<ID3D12Device8>;
+	using Factory = ComPtr<IDXGIFactory7>;
+	using Adapter = ComPtr<IDXGIAdapter4>;
+	using SwapChain = ComPtr<IDXGISwapChain4>;
+
+	// コマンド
+	using CmdList = ComPtr<ID3D12GraphicsCommandList6>;
+	using CmdQueue = ComPtr<ID3D12CommandQueue>;
+	using CmdAlloc = ComPtr<ID3D12CommandAllocator>;
+
+	// リソース・パイプライン
+	using Resource = ComPtr<ID3D12Resource>;
+	using Heap = ComPtr<ID3D12DescriptorHeap>;
+	using RootSig = ComPtr<ID3D12RootSignature>;
+	using PSO = ComPtr<ID3D12PipelineState>;
+	using Blob = ComPtr<ID3DBlob>;
+
+	// 同期デバッグ
+	using Fence = ComPtr<ID3D12Fence>;
+	using Debug = ComPtr<ID3D12Debug5>;
+}
